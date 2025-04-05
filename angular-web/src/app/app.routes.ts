3eb78b6912a0156707d6d54ed229266d471f83b6
daavid <davidruizdelcorro@outlook.com>
2025-04-05 11:02:41 +0200

    feat(web) Add pricing section

--- angular-web/src/app/app.routes.ts
+++ angular-web/src/app/app.routes.ts
@@ -5,9 +5,11 @@ import { WatchControlComponent } from './watch-control/watch-control.component';
 import { RedirectComponent } from './redirect/redirect.component';
 import { MacroCreateComponent } from './macro/create/macro-create.component';
 import { MacroMenuComponent } from './macro/menu/macro-menu.component';
+import { PricingComponent } from './pricing/pricing.component';
 
 export const routes: Routes = [
 	{ path: 'authenticate', component: AuthenticateComponent },
+	{ path: 'pricing', component: PricingComponent },
 	{ path: 'watch-control', component: WatchControlComponent },
 	{ path: 'redirect', component: RedirectComponent },
 	{ path: 'macro-menu', component: MacroMenuComponent },
